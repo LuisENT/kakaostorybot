b213ac3bdde161b600663e7de0c050bb2d98eae7
Luis P <ceo@luis-ent.info>
2016-03-08 10:26:23 +0900

    Update kakao.config.php

--- kakao.config.php
+++ kakao.config.php
@@ -1,5 +1,5 @@
 <?php
-	$kakao_app_id = "ba705877c150b73721b70629d09c930b"; // 카카오 개발자 센터의 REST API 키 입니다.
+	$kakao_app_id = " plz edit"; // 카카오 개발자 센터의 REST API 키 입니다.
 	$kakao_redirect = "http://localhost/kakao.process.php"; // KakaoStoryBot에서는 kakao.process.php 를 이용합니다. ex) http://yourserver.com/kakao.process.php
 	$kakao_get_token = "https://kauth.kakao.com/oauth/token"; 
 	$kakao_story_content = "TEST";
